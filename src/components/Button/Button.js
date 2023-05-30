d2564cb48a7e141bbcc37be95a5c72bac1b964d5
Benjamin Kwashie <benjaminkwashie@gmail.com>
2023-05-30 15:48:22 +0000

    refactor: transformed btn component to function component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,30 +1,26 @@
-import { Component } from 'react';
 import clsx from 'clsx';
 
 import './Button.css';
 
-class Button extends Component {
-   render() {
-      const {
-         name,
-         type = 'button',
-         onClick = () => {},
-         variant = 'primary',
-         disabled = false
-      } = this.props;
-      return (
-         <button
-            type={type}
-            onClick={() => onClick()}
-            className={clsx('reusable-btn', 'cursor-pointer', variant, {
-               disabled
-            })}
-            disabled={disabled}
-         >
-            {name}
-         </button>
-      );
-   }
+function Button({
+   name,
+   type = 'button',
+   onClick = () => {},
+   variant = 'primary',
+   disabled = false
+}) {
+   return (
+      <button
+         type={type}
+         onClick={() => onClick()}
+         className={clsx('reusable-btn', 'cursor-pointer', variant, {
+            disabled
+         })}
+         disabled={disabled}
+      >
+         {name}
+      </button>
+   );
 }
 
 export default Button;
